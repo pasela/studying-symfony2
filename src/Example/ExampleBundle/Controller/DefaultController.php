1613e1fedc44e835002c9f3a93c3e2ffd89e564d
Yuki <yukithm@gmail.com>
2012-06-15 18:07:48 +0900

    Implement Default:index action

--- src/Example/ExampleBundle/Controller/DefaultController.php
+++ src/Example/ExampleBundle/Controller/DefaultController.php
@@ -3,13 +3,18 @@
 namespace Example\ExampleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+    /**
+     * @Route("/", name="index")
+     * @Template
+     */
+    public function indexAction()
     {
-        return $this->render('ExampleExampleBundle:Default:index.html.twig', array('name' => $name));
+        return array('date' => date('Y-m-d H:i:s'));
     }
 }
